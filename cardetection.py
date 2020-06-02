2d7e43e0f93c5f6bed5972566c74f87aa951f91a
Thomas <callaghantw@gmail.com>
2020-06-02 13:12:58 -0400

    Starting to add windows to a small video. Still using frames that were split from a video to make it work, once all windows are working the video will be read in real time.

--- cardetection.py
+++ cardetection.py
@@ -2,6 +2,7 @@ import os
 import re
 import cv2 # opencv
 import numpy as np
+import time
 
 # get file names of the frames
 col_frames = os.listdir('./dji_0270_frames/')
@@ -51,40 +52,34 @@ for i in range(len(col_images) - 1):
     # find contours
     contours, hierarchy = cv2.findContours(dilated.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
 
-    # shortlist contours appearing in the detection zone
+    # countours
     valid_cntrs = []
     for cntr in contours:
         x, y, w, h = cv2.boundingRect(cntr)
-        if (x <= 80) & (y >= 80) & (cv2.contourArea(cntr) >= 25):
-            #if (y >= 90) & (cv2.contourArea(cntr) < 40):
+        # Setting the minimum size of something to be a contour
+        if (y <= 520) and cv2.contourArea(cntr) >= 1000:
             valid_cntrs.append(cntr)
 
     # add contours to original frames
     dmy = col_images[i].copy()
     cv2.drawContours(dmy, valid_cntrs, -1, (127, 200, 0), 2)
 
-    cv2.putText(dmy, "vehicles detected: " + str(len(valid_cntrs)), (55, 15), font, 0.6, (0, 180, 0), 2)
-    #cv2.line(dmy, (0, 80), (256, 80), (100, 255, 255))
-    #cv2.imwrite(pathIn + str(i) + '.png', dmy)
+    cv2.putText(dmy, "Vehicles detected: " + str(len(valid_cntrs)), (55, 15), font, 0.6, (0, 180, 0), 2)
 
-#frame_array = []
-
-#files = [f for f in os.listdir(pathIn) if isfile(join(pathIn, f))]
-#files.sort(key=lambda f: int(re.sub('\D', '', f)))
-
-#for i in range(len(files)):
-#    filename = pathIn + files[i]
-
-    # read frames
-#    img = cv2.imread(filename)
-#    height, width, layers = img.shape
-#    size = (width, height)
-
-    # inserting the frames into an image array
-#    frame_array.append(img)
-
-#for i in range(len(frame_array)):
-    # writing to a image array
     out.write(dmy)
+
+    # Regular image window
+
+    # Image difference window
+    cv2.namedWindow("Diff_image_window", cv2.WINDOW_NORMAL)
+    cv2.resizeWindow("Diff_image_window", 960, 540)
+    cv2.moveWindow("Diff_image_window", 960, 0)
+    cv2.imshow("Diff_image_window", diff_image)
+
+    # Thresh window
+
+    # Countour window
+
+    cv2.waitKey(1)
 
 out.release()
